cdddeb786464d0891efc1568d1fdcc011dba174f
Tom Deryckere <tom_deryckere@northps.com>
2011-02-12 15:44:12 -0500

    adding css and jquery

--- index.php
+++ index.php
@@ -3,11 +3,20 @@
 <title>
   Valentun.es | Passionate hackers
 </title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+<link rel="stylesheet" type="text/css" href="css/style.css" />
 </head>
 </html>
 <body>
+<div id="blurb">
+  
+</div>
 <h1>Valentunes</h1>
 
+<form>
+  <input type=text name="name"/>
+  <input type=text name="interests[0]" />
 
+</form>
 </body>
 
